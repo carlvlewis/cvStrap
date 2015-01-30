1279e47682fed39bb9305fc3347bdc96f6c28901
Carl V. Lewid <hello@carlvlew.is>
2015-01-30 01:55:27 -0500

    Updates to package.json

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var fs = require("fs");
 var Handlebars = require("handlebars");
 
 function render(resume) {
-var css = fs.readFileSync(__dirname + "/css/bootstrap.min.css", "utf-8")
-+ fs.readFileSync(__dirname + "css/shared.css", "utf-8")
-+ fs.readFileSync(__dirname + "css/screen.css", "utf-8")
-+ fs.readFileSync(__dirname + "css/print.css", "utf-8");
+var css = fs.readFileSync(__dirname + "/css/bootstrap.min.css", "utf-8") + fs.readFileSync(__dirname + "/css/shared.css", "utf-8") + fs.readFileSync(__dirname + "/css/screen.css", "utf-8") + fs.readFileSync(__dirname + "/css/print.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
 	return Handlebars.compile(template)({
 		css: css,
